967c802ff921db9a24890a2cc43852159a46aa05
Alexander <thebadgroover@gmail.com>
2020-11-28 18:45:22 +0300

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/chevron-right-solid.png"></button>'
+      });
+  });
